70203f7e9f697028ab7d6d2bda34ef2a60ca48d8
Richard Leo <leo.richard2@gmail.com>
2024-01-15 16:48:44 +0100

    Add cobj properties to get request

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PRIVATE_APP_ACCESS = process.env.HUBSPOT_KEY;
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 app.get('/', async (req, res) => {
-    const characters = 'https://api.hubspot.com/crm/v3/objects/2-22500567';
+    const characters = 'https://api.hubspot.com/crm/v3/objects/2-22500567?properties=name,power_level,type';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
